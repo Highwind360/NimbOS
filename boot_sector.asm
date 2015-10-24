3af2d84ee3d71215f63ecf950d3399846b4339a0
Grayson Sinclair <highwind360@gmail.com>
2015-10-24 16:00:34 -0700

    things things things

--- boot_sector.asm
+++ boot_sector.asm
@@ -1,6 +1,33 @@
-loop:
-	jmp loop
+[org 0x7c00]
 
-times 510 - ($ - $$) db 0
+mov ah, 0x0e ; for teletype mode
 
-dw 0Xaa55
+mov al, 'H' ; characters to be printed go in lower half of the register
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'o'
+int 0x10
+mov al, ' '
+int 0x10
+mov al, 'W'
+int 0x10
+mov al, 'o'
+int 0x10
+mov al, 'r'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'd'
+int 0x10
+
+jmp $ 		; loop to ensure program execution 
+			; doesn't tear off into the nether
+
+times 510 - ($ - $$) db 0 ; ensure program is 512 bytes
+
+dw 0Xaa55 ; magic number makes program a boot sector
