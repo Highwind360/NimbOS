6966e2db53b9fb0cc07c063e884baab6d2ac6a77
Grayson Sinclair <Highwind360@users.noreply.github.com>
2015-11-16 02:36:02 -0800

    fixed printing bug and compartmentalized functions in files

--- boot_sector.asm
+++ boot_sector.asm
@@ -10,48 +10,17 @@ call println
 mov bx, GOODBYE_MSG
 call print
 
-;
-;   Print function
-;   Takes pointer to a string via bx
-;   Returns void
-;   Preserves registers
-;   Uses bios interrupt routine 10,
-;   scrolling teletype
-;
-
-print:
-    push ax
-    push cx
-    mov ah, 0x0e        ; scrolling teletype routine
-.print_char:
-    mov cx, [bx]
-    test cx, cx
-    je .exit
-    add bx, 1
-    mov al, cl
-    int 0x10
-    jmp .print_char
-.exit:
-    pop cx
-    pop ax
-    ret
+jmp $
 
 ;
-;   Print with newline function
-;   Wrapper for print
-;   Appends newline
+;   Includes
 ;
 
-println:
-    call print
-    push bx
-    mov bx, NWLN
-    call print
-    pop bx
-    ret
+include "print_functions.asm"
 
-NWLN:
-    db 0xa,0xd,0
+;
+;   Data
+;
 
 GREETING_MSG:
     db 'Welcome to NimbOS. There is nothing here right now.',0
